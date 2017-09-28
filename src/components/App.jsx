e21421ac84d1ba6993005153ede46d9706b4f2f5
meza <meza@meza.hu>
2017-09-28 18:25:05 +0100

    added scss loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Hello World
+				Hello World 2
 			</div>
 		);
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 	template: './src/index.html',
 	filename: 'index.html',
@@ -12,19 +13,34 @@ module.exports = {
 		'webpack-dev-server/client?http://0.0.0.0:3000',
 		'webpack/hot/only-dev-server',
 		'react-hot-loader/patch',
-		'./src/index.js'
+		'./src/index.js',
+		'./src/stylesheets/main.scss'
 	],
 	output: {
 		path: path.resolve('dist'),
-		filename: 'index_bundle.js'
+		filename: 'index_bundle_[hash].js'
 	},
 	module: {
 		loaders: [
 			{test: /\.js$/, loaders: ['babel-loader', 'eslint-loader'], exclude: /node_modules/},
-			{test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
+			{test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+			{
+				test: /\.(sass|scss)$/,
+				loader: ExtractTextPlugin.extract([{loader: 'css-loader', options: { minimize: true }}, 'sass-loader'])
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			}
 		]
 	},
-	plugins: [HtmlWebpackPluginConfig],
+	plugins: [
+		HtmlWebpackPluginConfig,
+		new ExtractTextPlugin({ // define where to save the file
+			filename: '[name].[hash].bundle.css',
+			allChunks: true,
+		}),
+	],
 	devServer: {
 		port: 3000,
 		hot: true,
